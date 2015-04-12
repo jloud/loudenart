5b7c35a870113ba2a4d016a763626a5b24f0be4a
jloud <jim@loudenart.com>
2015-04-12 15:57:45 -0400

    fix some shit

--- wp-content/themes/loudart copy/cust-email-form.php
+++
@@ -1,6 +0,0 @@
-<form class="contact-form" method="post" action="<?php echo get_template_directory_uri(); ?>/cust-email-engine.php">
-	<input type="text" name="Name" id="Name" placeholder="Name"/>
-	<input type="text" name="Email" id="Email" placeholder="Email"/>
-	<textarea name="Message" rows="20" cols="20" id="Message" placeholder="Message"></textarea>
-	<input type="submit" name="submit" value="Send Message" class="submit-button" />
-</form>
